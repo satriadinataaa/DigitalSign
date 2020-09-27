b3814b816af0249665e173d6ea18c10ab6144c82
Satriadinata <satriadinata2@gmail.com>
2020-09-27 09:47:56 +0700

    icon

--- application/views/home/includes/header.php
+++ application/views/home/includes/header.php
@@ -6,7 +6,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
   <head>
-  
+  <link rel="icon" href="<?= base_url()?>assets/key.png">
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
