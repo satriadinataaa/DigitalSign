2b1ba76fe7fa13f0d85d3508022e3182ee052e9c
Satriadinata <satriadinata2@gmail.com>
2020-09-27 07:27:21 +0700

    rmv icon

--- application/views/home/includes/header.php
+++ application/views/home/includes/header.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <!doctype html>
 <html lang="en">
   <head>
-  <link rel="icon" href="<?= base_url()?>assets/logo-auladi.png">
+  
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
